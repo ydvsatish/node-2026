5cccdffbb9e136c24e29834a17b092e83dc8a9c4
“Satish <[satishyadavy196@gmail.com]>
2026-03-23 17:13:18 +0530

    get added

--- index.mjs
+++ index.mjs
@@ -2,6 +2,11 @@ import express from'express'
 const server = express()
 const port = 5000
 
+server.get("/", (req, res) => {
+  res.json({ name: "apple"})
+})
+
+
 server.listen(port, () => {
   console.log('server started on ${port}')
 })
