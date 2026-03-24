63bc667909d1f22911ded81831e35787aa47f845
“Satish <[satishyadavy196@gmail.com]>
2026-03-24 17:05:45 +0530

    server

--- index.mjs
+++ index.mjs
@@ -1,13 +1,22 @@
+import e from 'express'
 import express from'express'
 const server = express()
 const port = 5000
 
-server.get("/", (req, res) => {
-  res.json({ name: "apple"})
+server.use(express.json())
+
+server.post("/", (req, res) => {
+  res.status(200).json({
+    name: "apple",
+    position: "manger"
+  })
 })
 
-server.get("/gello", (req, res) => {
-  res.json({ name: "banana"})
+server.post("/login", (req, res) => {
+  console.log(req.body)
+  res.status(200).json({
+    message: "success"
+  })
 })
 
 server.listen(port, () => {
